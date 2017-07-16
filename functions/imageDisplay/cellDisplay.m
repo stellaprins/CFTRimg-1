c45f5cd3c802468a31d88a9f99cf170358ba33a2
heykayhay <heykayhay@users.noreply.github.com>
2017-07-16 12:29:30 +0100

    Add image and cell display functions.

---
+++ functions/imageDisplay/cellDisplay.m
@@ -0,0 +1,23 @@
+function cellDisplay(imageStruct, boundingBox_idx )
+%CELLDISPLAY Summary of this function goes here
+%   Detailed explanation goes here
+
+image = imread(imageStruct.path);
+boundingBox = imageStruct.boundingBox{boundingBox_idx};
+
+xmin = ceil(boundingBox(1));
+xmax = floor(boundingBox(1)) + boundingBox(3);
+ymin = ceil(boundingBox(2));
+ymax = floor(boundingBox(2)) + boundingBox(4);
+
+cropped = image(ymin:ymax,xmin:xmax);
+
+doubleImage = im2double(cropped);
+
+adjusted = imadjust(doubleImage,[min(doubleImage(:)); max(doubleImage(:))]...
+	,[0; 1]);
+
+imshow(adjusted)
+
+end
+
