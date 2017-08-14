1869d44fded4514286b9974c78ffa9929bd8feb5
heykayhay <heykayhay@users.noreply.github.com>
2017-08-14 15:47:23 +0100

    Minor changes. imgDisplay in color. Add BINNING to findBackground.

--- functions/imageDisplay/imgDisplay.m
+++ functions/imageDisplay/imgDisplay.m
@@ -5,9 +5,15 @@ function imgDisplay( imageStruct , color )
 
 switch color
 	case 'red'
-		image = imread(imageStruct.redPath);
+		image = imadjust(im2double(imread(imageStruct.redPath)));
+		[imX,map] = gray2ind(image,256);
+		redMap = [map(:,1),zeros(256,1),zeros(256,1)];
+		image = ind2rgb(imX,redMap);
 	case 'yel'
-		image = imread(imageStruct.yelPath);
+		image = imadjust(im2double(imread(imageStruct.yelPath)));
+		[imX,map] = gray2ind(image,256);
+		yelMap = [map(:,1),map(:,1),zeros(256,1)];
+		image = ind2rgb(imX,yelMap);
 	otherwise
 		disp('Please enter "red" or "yel" as the second argument of imgDisplay.')
 end
