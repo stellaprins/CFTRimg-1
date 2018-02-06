e4a73afe6d01d048b717b2278503272adfd5b58d
catohaste <catohaste@users.noreply.github.com>
2018-02-06 20:04:40 +0000

    Add in 2 overlay functions for imgDisplay (red and yellow).

--- functions/imageDisplay/imgDisplay.m
+++ functions/imageDisplay/imgDisplay.m
@@ -14,8 +14,24 @@ switch color
 		[imX,map] = gray2ind(image,256);
 		yelMap = [map(:,1),map(:,1),zeros(256,1)];
 		image = ind2rgb(imX,yelMap);
+	case 'blend'
+		yel = imadjust(im2double(imread(imageStruct.yelPath)));
+		red = imadjust(im2double(imread(imageStruct.redPath)));
+		[yelIm,map] = gray2ind(yel,256);
+		yelMap = [map(:,1),map(:,1),zeros(256,1)];
+		yelOut = ind2rgb(yelIm,yelMap);
+		[redIm,map] = gray2ind(red,256);
+		redMap = [map(:,1),zeros(256,1),zeros(256,1)];
+		redOut = ind2rgb(redIm,redMap);
+		image = imfuse(yelOut,redOut,'blend');
+	case 'combine'
+		yel = imadjust(im2double(imread(imageStruct.yelPath)));
+		red = imadjust(im2double(imread(imageStruct.redPath)));
+		image = zeros(2160,2160,3);
+		image(:,:,1) = red + yel;
+		image(:,:,2) = yel;
 	otherwise
-		disp('Please enter "red" or "yel" as the second argument of imgDisplay.')
+		disp('Please enter "red", "yel", "blend" or "combine" as the second argument of imgDisplay.')
 end
 
 imshow(image,[])
