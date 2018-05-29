e8816147fd3e86d3f37253a3fafc85c06b061df8
catohaste <catohaste@users.noreply.github.com>
2018-05-29 11:44:10 +0100

    Change filtering on cell dimensions to new tested criteria.

--- functions/imageLocal/imgFilterCellDimensions.m
+++ functions/imageLocal/imgFilterCellDimensions.m
@@ -1,10 +1,10 @@
 function [ imageStruct ] = imgFilterCellDimensions( imageStruct )
 %IMG_FILTER_CELL_DIMENSIONS Remove cells from analysis according to cell
 %dimensions
-%   Cells are removed if they are
-%			* too large
-%			* too small
-%			* have a large cell perimeter relative to their area
+%   Segmented components are removed if they have
+%			* too large an area
+%			* too long a length
+%			* a large cell perimeter relative to their area
 %		This last criterion ensures that selected cells are loosely circular.
 
 global BINNING
@@ -20,19 +20,28 @@ for idx=1:imageStruct.cellN(end)
 	cellMask = boundingBoxToCellMask(image,boundingBox);
 
 	[labelled, ~] = bwlabel(cellMask,8);
-	properties = regionprops(labelled ...
-		,'MinorAxisLength','MajorAxisLength','Area','Perimeter');
+	properties = regionprops(labelled,'Area','Perimeter','MajorAxisLength');
 	
 	if length(properties) == 1
-		cellLength = properties(1).MajorAxisLength;
-		cellWidth = properties(1).MinorAxisLength;
 		
+		perimeterMask = cellMask;
+		perimeterMask(:) = 0;
+		perimeterMask(:,1) = 1;
+		perimeterMask(:,end) = 1;
+		perimeterMask(1,:) = 1;
+		perimeterMask(end,:) = 1;
+		edgeMask = cellMask & perimeterMask;
+		
+		edgeProportion = sum(edgeMask(:)) / sum(perimeterMask(:));
+		cellLength = properties(1).MajorAxisLength;
 		cellArea = properties(1).Area;
 		cellPerimeter = properties(1).Perimeter;
 		
-				cellLogical(idx) = cellLength < 300*BINNING ...
-											& 50*BINNING < cellWidth ...
-											& cellArea/cellPerimeter > 20*BINNING;
+		
+				cellLogical(idx) = cellArea/cellPerimeter > 25*BINNING...
+											& cellLength < 300*BINNING...
+											& cellArea < 40000*BINNING*BINNING...
+											& edgeProportion < 0.3;
 
 	end
 
