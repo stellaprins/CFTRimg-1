4ea043727ecff3083e2259742d7c6058789c3cd3
catohaste <catohaste@users.noreply.github.com>
2018-07-09 08:27:33 +0100

    Update variable names, isolate localization structs from quenching, change naming of exp and plate.

--- functions/structsStartData/findImagePaths_local.m
+++ functions/structsStartData/findImagePaths_local.m
@@ -1,42 +1,45 @@
-function [ conditionArray,redPathArray,yelPathArray ] = ...
-	findImagePathsLocal(plateStruct,conditionArray,redPathArray,yelPathArray)
+function [ conditionArray,binningArray,redPathArray,yelPathArray ] = ...
+	findImagePaths_local(inputStruct,conditionArray,binningArray,...
+	redPathArray,yelPathArray)
 %FIND_IMAGE_PATHS_LOCAL construct path names for localization images
 %   Add to 'redPathArray' and 'yelPathArray' new path names constructed for
 %   each localization image. Done for all wells in an experiment.
 
-fileFolder = fullfile(plateStruct.baseFolder...
-	,plateStruct.folderName...
+fileFolder = fullfile(inputStruct.baseFolder...
+	,inputStruct.folderName...
 	,'TimePoint_1');
 
-wells = {plateStruct.condWells{:}};
+wells = {inputStruct.condWells{:}};
 emptyCells = cellfun('isempty',wells);
 wells(emptyCells) = [];
 wellN = length(wells);
-siteN = plateStruct.localSiteN;
+siteN = inputStruct.siteN;
 
 tmpConditionArray	= cell(wellN*siteN,1);
+tmpBinningArray		= cell(wellN*siteN,1);
 tmpRedPathArray		= cell(wellN*siteN,1);
 tmpYelPathArray		= cell(wellN*siteN,1);
 
 for j=1:wellN
 
 	% red
-	filename			= strcat(plateStruct.filePrefix,wells{j},'_s*_w2.TIF');
+	filename			= strcat(inputStruct.filePrefix,wells{j},'_s*_w2.TIF');
 	redDirOutput	= dir(fullfile(fileFolder,filename));
 		if isempty(redDirOutput)
 			fprintf('Check experiment "%s".\nFor well %s a file cannot be found.\n',...
-			plateStruct.folderName,wells{j})
+			inputStruct.folderName,wells{j})
 		end
 
 	% yellow
-	filename			= strcat(plateStruct.filePrefix,wells{j},'_s*_w1.TIF');
+	filename			= strcat(inputStruct.filePrefix,wells{j},'_s*_w1.TIF');
 	yelDirOutput	= dir(fullfile(fileFolder,filename));
 	
 	% find current condition for this well
-	locationMatrix = strcmp(wells{j},plateStruct.condWells);
-	for i=1:size(plateStruct.condWells,1)
+	locationMatrix = strcmp(wells{j},inputStruct.condWells);
+	for i=1:size(inputStruct.condWells,1)
 		if sum(locationMatrix(i,:)) == 1
-			currentCondition = plateStruct.conditionStr{i};
+			currentCondition	= inputStruct.conditionStr{i};
+			currentBinning		= inputStruct.pixelBinning;
 		end
 	end
 
@@ -44,6 +47,8 @@ for j=1:wellN
 	for p = 1:siteN
 		tmpConditionArray{(j-1)*siteN + p} = ...
 			currentCondition;
+		tmpBinningArray{(j-1)*siteN + p} = ...
+			currentBinning;
 		tmpRedPathArray{(j-1)*siteN + p} = ...
 			fullfile(fileFolder,redDirOutput(p).name);
 		tmpYelPathArray{(j-1)*siteN + p} = ...
@@ -54,6 +59,7 @@ for j=1:wellN
 end
 
 conditionArray	= [conditionArray; tmpConditionArray];
+binningArray		= [binningArray; tmpBinningArray];
 redPathArray		= [redPathArray; tmpRedPathArray];
 yelPathArray		= [yelPathArray; tmpYelPathArray];
 
